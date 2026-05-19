d4e9fa0d9fbcccbc1e55476d56dbc68374062371
Julian Miguel I. Felipe <felipejulianmiguel@gmail.com>
2026-05-19 08:39:57 +0800

    git commit -m "fix AppShell casing"

--- src/components/layout/Appshell.jsx
+++ src/components/layout/Appshell.jsx
@@ -1,7 +1,7 @@
 import { Outlet } from 'react-router-dom'
 import Navbar from './Navbar'
 import Sidebar from './Sidebar'
-
+// No row yet
 export default function AppShell() {
   return (
     <div className="flex flex-col h-screen bg-gray-50">
